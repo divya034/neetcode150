602e2f0063170f1f368972914475cd47600baec2
divya034 <59786992+divya034@users.noreply.github.com>
2024-12-10 01:55:01 +0530

    Create summary.cpp

---
+++ graphs/summary.cpp
@@ -0,0 +1,8 @@
+1. Number of islands:
+BFS, DFS, and disjoint union
+-if we find 1, count++ 
+call a bfs/dfs call, mark all 1s around it as vis, 
+
+-go on traversing the grid for next unvisited "1" and count++ so on.
+
+
