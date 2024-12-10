421336eb8ea742b00e0b24233a6873d803dbf02a
divya034 <59786992+divya034@users.noreply.github.com>
2024-12-10 19:55:43 +0530

    Update max_area_of_island.cpp

--- graphs/max_area_of_island.cpp
+++ graphs/max_area_of_island.cpp
@@ -1,3 +1,4 @@
+BFS:
 class Solution {
 public:
     int bfs(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &vis){
@@ -55,3 +56,59 @@ public:
         return max_area;
     }
 };
+
+
+DFS:
+class Solution {
+        vector<int> dx = {-1, 1, 0, 0};
+        vector<int> dy = {0, 0, -1, 1};
+public:
+    int dfs(int i , int j, vector<vector<int>> &vis, vector<vector<int>> &grid, int &area){
+        vis[i][j] = 1;
+        int r = grid.size();
+        int c = grid[0].size();
+
+        for(int k = 0; k<4; k++){
+            int new_x = i + dx[k];
+            int new_y = j + dy[k];
+
+            if(new_x >= 0 && new_x < r &&
+                new_y >= 0 && new_y < c &&
+                grid[new_x][new_y] == 1 && vis[new_x][new_y] != 1){
+                    area = area + 1;
+                    dfs(new_x, new_y, vis, grid, area);
+            }
+        }
+
+        return area;
+
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        
+        int r = grid.size();
+        int c = grid[0].size();
+        int max_area = 0;
+        vector<vector<int>> vis(r, vector<int>(c, 0));
+
+        for(int i = 0; i<r; i++){
+            for(int j = 0; j<c; j++){
+                if(grid[i][j] == 1 && vis[i][j] == 0){
+                    int area = 1;
+                    area = dfs(i, j, vis, grid, area);
+                    max_area = max(area, max_area);
+                }
+            }
+        }
+        return max_area;
+    }
+};
+// approach
+// when we find a 1--->
+// call dfs/bfs get the area, mark everything visited in path
+// max_area = max(area, max_area);
+
+// initial:
+// max_area = 0;
+
+
+
